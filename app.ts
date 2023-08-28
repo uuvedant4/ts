293907217fdfe1da7105fe0b24e999119c40919a
vedant <ursvedantyetekar@gmail.com>
2023-08-28 20:52:33 +0530

    enum and tuple

--- app.ts
+++ app.ts
@@ -10,3 +10,25 @@ console.log(add(num1, num2));
 // const names: any[] = ["Vedant", "Parth", "John", 4];
 const names: string[] = ["Vedant", "Parth", "John"];
 console.log(names);
+
+// Enum
+
+enum SDE_LEVEL {
+  I = 1,
+  II = 2,
+  III = 3,
+}
+
+const userDetails: {
+  name: string;
+  hobbies: string[];
+  age: number;
+  role: [string, number]; // tuple
+} = {
+  name: "Vedant Yetekar",
+  hobbies: ["music", "coding"],
+  age: 21,
+  role: ["SDE", SDE_LEVEL.I],
+};
+
+userDetails.role[1] = 1;
