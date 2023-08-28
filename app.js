65af1263f8eeda1aff074f3e9e4cda27f625208f
vedant <ursvedantyetekar@gmail.com>
2023-08-28 16:29:18 +0530

    typescript basics

---
+++ app.js
@@ -0,0 +1,9 @@
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+var num1 = 3;
+var num2 = 4;
+console.log(add(num1, num2));
+// const names: any[] = ["Vedant", "Parth", "John", 4];
+var names = ["Vedant", "Parth", "John"];
+console.log(names);
---
+++ app.ts
@@ -0,0 +1,12 @@
+const add = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+const num1: number = 3;
+const num2: number = 4;
+
+console.log(add(num1, num2));
+
+// const names: any[] = ["Vedant", "Parth", "John", 4];
+const names: string[] = ["Vedant", "Parth", "John"];
+console.log(names);
