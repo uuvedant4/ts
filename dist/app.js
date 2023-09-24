d12534765f890df87b056bfde7e35d300864f447
vedant <ursvedantyetekar@gmail.com>
2023-09-24 15:44:49 +0530

    classes

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+class Student {
+    constructor(studentsName) {
+        this.studentsName = studentsName;
+    }
+}
+const studentObject = new Student("Vedant");
+console.log(studentObject);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
-const btn = document.querySelector("button")!;
+// classes and interfaces
+class Student {
+  studentsName: String;
+  constructor(studentsName: string) {
+    this.studentsName = studentsName;
+  }
+}
 
-btn.addEventListener("click", () => {
-  console.log("clicked");
-});
+const studentObject = new Student("Vedant");
+console.log(studentObject);
+
+// 61
